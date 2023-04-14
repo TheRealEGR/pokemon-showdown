3e2f9589f47b10094b058f2629c3364edf0d41ba
TheRealEGR <129534891+TheRealEGR@users.noreply.github.com>
2023-04-14 21:18:55 +0200

    Add files via upload

--- config/formats.ts
+++ config/formats.ts
@@ -212,6 +212,21 @@ export const Formats: FormatList = [
 		ruleset: ['Standard', 'Freeze Clause Mod'],
 		banlist: ['Uber', 'Sand Veil', 'Assist', 'Baton Pass'],
 	},
+		{
+		name: "[Gen 3] CrossGen Ubers",
+
+		mod: 'gen3crossgen',
+		ruleset: ['Standard', 'Freeze Clause Mod'],
+		banlist: ['Sand Veil', 'Assist', 'Baton Pass'],
+	},
+		{
+		name: "[Gen 3] CrossGen UU",
+
+		mod: 'gen3crossgen',
+		ruleset: ['Standard', 'Freeze Clause Mod'],
+		banlist: ['Uber', 'OU', 'UUBL' 'Sand Veil', 'Assist', 'Baton Pass'],
+	},
+	
 	
 	// S/V Doubles
 	///////////////////////////////////////////////////////////////////
