3942406374d9e4d944eb214f27e8a8819fc34fe9
TheRealEGR <129534891+TheRealEGR@users.noreply.github.com>
2023-04-15 23:17:58 +0200

    Add files via upload

---
+++ data/custom-formats.ts
@@ -0,0 +1,27 @@
+export const Formats: FormatList = [
+	
+	// Custom Formats 
+	///////////////////////////////////////////////////////////////////
+	
+	{
+		name: "[Gen 3] CrossGen OU",
+
+		mod: 'gen3crossgen',
+		ruleset: ['Standard', 'Freeze Clause Mod'],
+		banlist: ['Uber', 'Sand Veil', 'Assist', 'Baton Pass'],
+	},
+		{
+		name: "[Gen 3] CrossGen Ubers",
+
+		mod: 'gen3crossgen',
+		ruleset: ['Standard', 'Freeze Clause Mod'],
+		banlist: ['Sand Veil', 'Assist', 'Baton Pass'],
+	},
+		{
+		name: "[Gen 3] CrossGen UU",
+
+		mod: 'gen3crossgen',
+		ruleset: ['Standard', 'Freeze Clause Mod'],
+		banlist: ['Uber', 'OU', 'UUBL', 'Sand Veil', 'Assist', 'Baton Pass'],
+	},
+];
